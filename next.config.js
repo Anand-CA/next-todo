2e001fe5f4462dd400d01c3c2fb15f5fa8958e8d
anand <anandca.an7@gmail.com>
2021-07-20 21:15:55 +0530

    next config removed

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    MONGODB_URI:
-      "mongodb+srv://test:test123@cluster0.ayqot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    MONGODB_DB: "todo",
-    BASE_URL: "http://localhost:3000/api/todo",
-  },
-};
